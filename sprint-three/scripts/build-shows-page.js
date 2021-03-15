5e400a8a7b4e18dd0c5f4a8926abd9417bcdff6c
sweetart-hub <77159974+sweetart-hub@users.noreply.github.com>
2021-03-14 23:48:19 -0400

    footer and partial shows section add

---
+++ sprint-three/scripts/build-shows-page.js
@@ -0,0 +1,9 @@
+let form = document.getElementById(".shows-list__form");
+
+let showsArray = [
+    {date: "Mon Dec 17 2018", venue: "Ronald Lane", location: "San Francisco, CA"} , {date: "Wed Aug 11 2019", venue: "Pres Club", location: "San Francisco, CA"} , {date: "Tue Jul 18 2019", venue: "Pier 3 East", location: "San Francisco, CA"} , {date: "Fri July 22 2019", venue: "View Lounge", location: "San Francisco, CA"} , {date: "Sat Aug 12 2019", venue: "Hyatt Agency", location: "San Francisco, CA"} , {date: "Fri Sep 05 2019", venue: "Moscow Center", location: "San Francisco, CA"} 
+];
+
+<input type="submit" value="Buy Tickets"></input>
+
+//form.addEventListener("submit", (event) =>//
